021438e1c160f976f6319b93aca4f80ac07c7010
chmullerweb <ch.muller.web@gmail.com>
2020-05-27 17:32:22 +0200

    Initialisation du projet CV
    Grid
    Feuille de style css + reset css

--- index.php
+++ index.php
@@ -4,9 +4,28 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Charlotte_Muller_CV</title>
+
+    <!-- ////   CSS   //// -->
+    <link rel="stylesheet" href="http://localhost/coursphp/CV/css/cssreset.css">
+    <link rel="stylesheet" href="http://localhost/coursphp/CV/css/grid.css">
+
+    <!-- ////   PATTERN   //// -->
+    <link href="https://unpkg.com/pattern.css" rel="stylesheet">
+
+    <!-- //// FONT ////-->
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,200;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Aladin&display=swap" rel="stylesheet">
+
 </head>
 
-<body>
+
+<body class="grid">
+    
+    <!--header--> 
+    
+    <div class="header row1 col3">
+                
+    </div>
     
 </body>
 </html>
